448dabdffd52efcfe24918a0cd15899579ebe0d8
Chris Leja <christopher.leja@gmail.com>
2020-07-09 10:19:36 -0700

    minor layout tweaks, copy edits to resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,8 +11,8 @@ const Footer = () => {
         </div>
         <div className="footer__credit">
           <p> Background video on Home page is by <a className="footer__credit-link" href="https://www.pexels.com/@enginakyurt?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels" target="_blank" rel="noopener noreferrer">Engin Akyurt</a></p>
-          <p> Background art on Resume page is by <a className="footer__credit-link" href="https://unsplash.com/@anniespratt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">Annie Spratt</a></p>
-          <p> Background art on About page is by <a className="footer__credit-link" href="https://unsplash.com/@steve_j?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">Steve Johnson</a></p>
+          <p> Background art on Resume page by <a className="footer__credit-link" href="https://unsplash.com/@anniespratt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">Annie Spratt</a></p>
+          <p> Background art on About page by <a className="footer__credit-link" href="https://unsplash.com/@steve_j?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">Steve Johnson</a></p>
         </div>
         <div className="footer__links">
           <Subheading />
